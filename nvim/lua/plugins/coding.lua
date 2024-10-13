43695acd0b5c440f0ddeb4a9b4916f87b0dfd7b5
Anton Golosnichenko <a.golosnichenko@icloud.com>
2024-10-14 01:03:08 +0500

    feat: add avante for nvim

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,12 +1,51 @@
 return {
     {
-        "Exafunction/codeium.nvim",
+        "yetone/avante.nvim",
+        event = "VeryLazy",
+        lazy = false,
+        version = false, -- set this if you want to always pull the latest change
         opts = {
-            enable_chat = true,
+            provider = "claude",
+            claude = {
+                api_key_name = "ANTHROPIC_API_KEY",
+            },
         },
-        keys = {
-            { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
-            { "<leader>aa", "<cmd>Codeium Chat<cr>", desc = "Toggle chat (Codeium)", mode = { "n", "v" } },
+        -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+        build = "make",
+        -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "stevearc/dressing.nvim",
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            --- The below dependencies are optional,
+            "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+            "zbirenbaum/copilot.lua", -- for providers='copilot'
+            {
+                -- support for image pasting
+                "HakonHarnes/img-clip.nvim",
+                event = "VeryLazy",
+                opts = {
+                    -- recommended settings
+                    default = {
+                        embed_image_as_base64 = false,
+                        prompt_for_file_name = false,
+                        drag_and_drop = {
+                            insert_mode = true,
+                        },
+                        -- required for Windows users
+                        use_absolute_path = true,
+                    },
+                },
+            },
+            {
+                -- Make sure to set this up properly if you have lazy=true
+                "MeanderingProgrammer/render-markdown.nvim",
+                opts = {
+                    file_types = { "markdown", "Avante" },
+                },
+                ft = { "markdown", "Avante" },
+            },
         },
     },
     {
@@ -28,7 +67,7 @@ return {
                 ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-                        cmp.confirm({ select = true })
+                        cmp.select_next_item()
                     elseif vim.snippet.active({ direction = 1 }) then
                         vim.schedule(function()
                             vim.snippet.jump(1)
